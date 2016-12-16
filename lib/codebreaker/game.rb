b4afdaea773aef6844b1fceb2e3dcd0a462914b3
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-12-16 23:56:23 +0200

    Complete Game.start

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,11 +1,25 @@
 module Codebreaker
+  QUANTITY_GUESSES = 7
+  QUANTITY_HINTS = 3
   class Game
     def initialize
       @secret_code = ''
+      @guesses_quantity = nil
+      @hints_quantity = nil
     end
 
     def start
-      @secret_code = '1256'
+      @secret_code = generate_secret
+      @guesses_quantity = QUANTITY_GUESSES
+      @hints_quantity = QUANTITY_HINTS
     end
+
+    private
+
+      def generate_secret(capacity = 4)
+        result = []
+        capacity.times { result << rand(1..6) }
+        result.join
+      end
   end
 end
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,22 +2,54 @@ require 'spec_helper'
 
 module Codebreaker
   RSpec.describe Game do
+    subject { Game.new }
+
     describe '#start' do
-      let(:game) { Game.new }
       before do
-        game.start
+        subject.start
       end
 
-      it 'generates secret code' do        
-        expect(game.instance_variable_get(:@secret_code)).not_to be_empty
+      context 'does with secret code:' do
+        let(:secret_code) { subject.instance_variable_get(:@secret_code) }
+        it 'generates' do        
+          expect(secret_code).not_to be_empty
+        end
+
+        it 'saves 4 numbers' do
+          expect(secret_code).to have(4).items
+        end
+
+        it 'saves with numbers from 1 to 6' do
+          expect(secret_code).to match(/[1-6]{4}/)
+        end
+
+        it 'generate different from game to game' do
+          expect { subject.start }.to change{subject.instance_variable_get(:@secret_code) }
+        end
       end
 
-      it 'saves 4 numbers secret code' do
-        expect(game.instance_variable_get(:@secret_code)).to have(4).items
+      context 'does with quantity of guesses:' do
+        let(:guesses_quantity) { subject.instance_variable_get(:@guesses_quantity) }
+
+        it 'generates' do        
+          expect(guesses_quantity).not_to be_nil
+        end
+
+        it 'get value from constant' do
+          expect(guesses_quantity).to eq(Codebreaker::QUANTITY_GUESSES)
+        end
       end
 
-      it 'saves secret code with numbers from 1 to 6' do
-        expect(game.instance_variable_get(:@secret_code)).to match(/[1-6]{4}/)
+      context 'does with quantity of hints:' do
+        let(:hints_quantity) { subject.instance_variable_get(:@hints_quantity) }
+
+        it 'generates' do        
+          expect(hints_quantity).not_to be_nil
+        end
+
+        it 'get value from constant' do
+          expect(hints_quantity).to eq(Codebreaker::QUANTITY_HINTS)
+        end
       end
     end
   end
