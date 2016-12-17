52fd7b3b0f963a0d2d66625ad3a2f69b279a7276
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-12-18 00:29:52 +0200

    Complete methods: win?, lose?, check_guess, score

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -3,6 +3,8 @@ module Codebreaker
   QUANTITY_HINTS = 3
   CODE_CAPACITY = 4
   MESSAGE_HINTS_ARE_USED = 'You have used all hints!'
+  BEST_GUESS_MARK = '++++'
+  SCORE_TEMPLATE = 'secret is _s | attemts is _a | hints are used _h'
   class Game
     def initialize
     end
@@ -23,13 +25,60 @@ module Codebreaker
     end
 
     def submit_guess(guess)
+      @guesses_quantity -= 1 
+      check_guess(guess.chars)
+    end
 
+    def win?(pluses_minuses)
+      pluses_minuses == BEST_GUESS_MARK
+    end
+
+    def lose?(pluses_minuses)
+      pluses_minuses != BEST_GUESS_MARK && @guesses_quantity == 0
+    end
+
+    def score
+      SCORE_TEMPLATE.sub(/_s/, "#{@secret_code}")
+                    .sub(/_a/, "#{QUANTITY_GUESSES - @guesses_quantity}")
+                    .sub(/_h/, "#{QUANTITY_HINTS - @hints_quantity}")
     end
 
     private
 
-      def generate_secret(capacity = CODE_CAPACITY)        
-        Array.new(capacity).map { rand(1..6) }.join
+      def generate_secret        
+        Array.new(CODE_CAPACITY).map { rand(1..6) }.join
+      end
+
+      def check_guess(guess)  
+        answer = delete_false_minuses(delete_false_pluses(guess))
+        answer.compact.sort.join
+      end
+
+      def delete_false_pluses(guess)
+        answer = Array.new(CODE_CAPACITY,'+')
+        answer.each_index do |index|
+          if guess[index] != @secret_code[index]
+            answer[index] = '-' 
+          else
+            guess[index] = nil
+          end
+        end
+        [answer, guess]
+      end
+
+      def delete_false_minuses(answer_and_guess)
+        answer, guess = answer_and_guess
+        answer.each_index do |index|
+          if answer[index] == '-'
+            in_guess_index = guess.index(@secret_code[index])
+            if in_guess_index
+              guess[in_guess_index] = nil
+            else
+              answer[index] = nil
+            end
+          end
+        end
+        answer
       end
   end
 end
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -78,12 +78,107 @@ module Codebreaker
       end
 
       it 'should change quantity of guesses' do
-        expect { subject.submit_guess(1234) }.to change{ subject.instance_variable_get(:@guesses_quantity) }.by(-1)
+        expect { subject.submit_guess('1234') }.to change{ subject.instance_variable_get(:@guesses_quantity) }.by(-1)
       end
 
-      it 'should return string equell or less than CODE_CAPACITY'
       context 'check answers' do
+        [
+          ['1111', '1111', '++++'],
+          ['1111', '1112', '+++'],
+          ['1111', '1211', '+++'],
+          ['1121', '1211', '++--'],
+          ['1121', '3211', '+--'],
+          ['1112', '1234', '+-'],
+          ['1121', '1234', '+-'],
+          ['1122', '1324', '++'],
+          ['1122', '2211', '----'],
+          ['1212', '1234', '++'],
+          ['1212', '1221', '++--'],
+          ['1211', '3456', ''],
+          ['1234', '4321', '----'],
+          ['1222', '2111', '--'],
+          ['1222', '2121', '+--'],
+            ['9876','6789','----'],
+          ['0135','1035','++--'],
+          ['2363', '2366', '+++'],
+          ['1234', '2345', '---'],
+          ['3635', '3333', '++'],
+          ['1234', '5513', '--'],
+          ['1234', '5535', '+'],
+          ['1234', '5525', '-'],
+          ['1234', '4213', '+---'],
+          ['1234', '2134', '++--'],
+          ['1234', '5134', '++-'],
+          ['1234', '5213', '+--'],
+          ['1234', '1546', '+-'],
+          ['1234', '5555', ''],
+          ['1221','2332','--'],
+          ['1222','2431','--'],
+          ['1222','2435','-'],
+          ['0135','1035','++--'],
+          ['2222','2222','++++'],  
+        ].each do |line|
+          it "get secret is <#{line.first}>, guess is <#{line[1]}>, should get <#{line.last}>" do
+            subject.instance_variable_set(:@secret_code, line.first)
+            expect(subject.submit_guess(line[1])).to eq(line.last)
+          end
+        end
+      end
+    end
 
+    describe '#win?' do
+      before do
+        subject.start
+      end
+
+      it 'should return true if it get "++++"' do
+        expect(subject.win? '++++').to be_truthy
+      end
+
+      it 'should be false if it get any string except "++++"' do
+        expect(subject.win? '+++-').to be_falsey
+        expect(subject.win? '----').to be_falsey
+        expect(subject.win? 'Some string').to be_falsey
+      end
+    end
+
+    describe '#lose?' do
+      before do
+        subject.start
+      end
+
+      it 'should return true' do
+        subject.instance_variable_set(:@guesses_quantity, 0)
+        expect(subject.lose? '+++-').to be_truthy
+      end
+
+      context 'should return false' do
+        specify 'guesses quantity is 0 and answer is "++++"' do
+          subject.instance_variable_set(:@guesses_quantity, 0)
+          expect(subject.lose? '++++').to be_falsey
+        end
+
+        specify 'guesses quantity grate than 0 and answer is "++++"' do
+          subject.instance_variable_set(:@guesses_quantity, 4)
+          expect(subject.lose? '++++').to be_falsey
+        end
+
+        specify 'guesses quantity grate than 0 and answer is "++-"' do
+          subject.instance_variable_set(:@guesses_quantity, 4)
+          expect(subject.lose? '++-').to be_falsey
+        end
+      end
+    end
+
+    describe '#score' do
+      before do
+        subject.start
+      end
+
+      it 'should return right string with info' do
+        subject.instance_variable_set(:@guesses_quantity, 4)
+        subject.instance_variable_set(:@hints_quantity, 1)
+        expect(subject.score).to match(/secret is \d{4} | attemts is 3 | hints are used 2/)
       end
     end
   end
