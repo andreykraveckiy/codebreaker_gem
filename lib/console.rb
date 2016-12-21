fc393826c302ba83915e74c1f5370abbb02fd0a5
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-12-21 03:41:16 +0200

    Review GameProcess. Add write and load file.yaml

--- lib/console.rb
+++ lib/console.rb
@@ -1,32 +1,66 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 require 'codebreaker'
 
-def fire
-  puts %{
-    Welcome to the codebreaker!
-    This amazing game proposes 
-    you to guess 4 digit number. 
-    You have #{Codebreaker::QUANTITY_GUESSES} attempts
-    and #{Codebreaker::QUANTITY_HINTS} hints. Use last wisely!!!
-    Good luck!
-  }
+module Codebreaker
+  class Console
+    def self.run
+      puts %{
+        Welcome to the codebreaker!
+        This amazing game proposes 
+        you to guess 4 digit number. 
+        You have #{Codebreaker::QUANTITY_GUESSES} attempts
+        and #{Codebreaker::QUANTITY_HINTS} hints. Use last wisely!!!
+        Good luck!
+      }
 
-  game = Codebreaker::GameProcess.new
-  puts "Please type 'new game' for begining."
-  while true
-    command = $stdin.gets.chomp.downcase
-    exit if command == 'q'
-    respond = game.bottleneck(command)
-    exit if respond == 'exit'
-    if respond.match(/YOU/)
-      puts 'Would you like to play again?[yes/no]'
+      game = Codebreaker::GameProcess.new
+      puts generate_template(:menu)
+      while true
+        command = $stdin.gets.chomp.downcase
+        respond = game.listens_and_shows(command)
+        if respond.is_a?(Symbol)
+          puts generate_template(respond)
+          game.answers.each { |score| puts score } if respond == :scores
+        else
+          puts game.answers
+        end
+      end
     end
-    puts respond
-    if command.match(/^[1-6]{4}$/) && !respond.match(/YOU/)
-      puts "You have only #{game.current_guess} attempts. Type your next command:" 
+
+    def self.generate_template(template)
+      case template
+      when :menu
+        %{
+          Type: 'new game' for begining;
+                'scores' to see achieemnts;
+                'exit' for exit.
+        }
+      when :game
+        %{
+          Type: 'hint' to see hint;         
+                your guess - 4 with numbers from 1 to 6;
+                'new game' for brestart.
+        }
+      when :scores
+        %{
+          There are achievments of COdebreaker.
+          Type 'back' to back in main menu.
+        }
+      when :complete_game
+        %{
+          Wold you like to save your score?(yes/no)
+        }
+      when :save_score
+        %{
+          Input your name.
+        }
+      when :repete
+        %{
+          Would you like to play again?(yes/no)
+        }
+      when :exit
+        exit
+      end
     end
-    puts "You have only #{game.current_hint} hints." if command == 'hint'
   end
 end
-
-fire
